6e6567c220e894e5224472cd4e06de254b2a76b1
augustine-a8 <logos.agebeko@gmail.com>
2022-09-04 10:13:17 -0700

    create voter modal selects fixed

--- src/components/CreateVoterModal.js
+++ src/components/CreateVoterModal.js
@@ -10,8 +10,10 @@ import {
   Input,
   FormLabel,
   Box,
+  Select,
   Button,
 } from "@chakra-ui/react";
+import { BsCaretDown } from "react-icons/bs";
 
 export default function CreateVoterModal({ isOpen, onClose }) {
   return (
@@ -33,7 +35,20 @@ export default function CreateVoterModal({ isOpen, onClose }) {
               </FormControl>
               <FormControl>
                 <FormLabel>College</FormLabel>
-                <Input />
+                <Select placeholder="Select option" icon={<BsCaretDown />}>
+                  <option value="option1">
+                    College of Agriculture and Natural Resources
+                  </option>
+                  <option value="option2">
+                    College of Art and Built Environment
+                  </option>
+                  <option value="option3">
+                    College of Humanities and Social Sciences
+                  </option>
+                  <option value="option4">College of Engineering</option>
+                  <option value="option5">College of Health Sciences</option>
+                  <option value="option6">College of Science</option>
+                </Select>
               </FormControl>
               <FormControl>
                 <FormLabel>Student ID</FormLabel>
@@ -41,7 +56,14 @@ export default function CreateVoterModal({ isOpen, onClose }) {
               </FormControl>
               <FormControl>
                 <FormLabel>Year</FormLabel>
-                <Input />
+                <Select placeholder="Select option" icon={<BsCaretDown />}>
+                  <option value="option1">Year One</option>
+                  <option value="option2">Year Two</option>
+                  <option value="option3">Year Three</option>
+                  <option value="option3">Year Four</option>
+                  <option value="option3">Year Five</option>
+                  <option value="option3">Year Six</option>
+                </Select>
               </FormControl>
             </ModalBody>
           </Box>
